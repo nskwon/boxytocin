3a29c7cda61657b285ddc99a2dd6639403517c2f
nskwon <nskwon@ucsd.edu>
2020-02-27 03:53:56 -0800

    Optimized Collision between players
    Removed sliding due to collisions

--- Boxytocin Project/Assets/Scripts/Player2Movement.cs
+++ Boxytocin Project/Assets/Scripts/Player2Movement.cs
@@ -7,10 +7,13 @@ public class Player2Movement : MonoBehaviour
     public float moveSpeed = 5.0f;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private BoxCollider2D box;
+    private bool canMove;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -18,15 +21,45 @@ public class Player2Movement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal2");
         movement.y = Input.GetAxisRaw("Vertical2");
+        canMove = CheckCollisions(box, movement, 0.05f);
     }
 
     private void FixedUpdate()
     {
-        if (movement.x != 0 && movement.y != 0)
+        if (canMove)
         {
-            rb.MovePosition(rb.position + movement * (moveSpeed * 0.7f) * Time.fixedDeltaTime);
+            movement.x = 0;
+            movement.y = 0;
         }
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        else
+        {
+
+            if (movement.x != 0 && movement.y != 0)
+            {
+                rb.MovePosition(rb.position + movement * (moveSpeed * 0.7f) * Time.fixedDeltaTime);
+            }
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        }
+    }
+
+    private bool CheckCollisions(Collider2D moveCollider, Vector2 direction, float distance)
+    {
+        if (moveCollider != null)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[10];
+            ContactFilter2D filter = new ContactFilter2D() { };
+
+            int numHits = moveCollider.Cast(direction, filter, hits, distance);
+            for (int i = 0; i < numHits; i++)
+            {
+                if (!hits[i].collider.isTrigger)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
