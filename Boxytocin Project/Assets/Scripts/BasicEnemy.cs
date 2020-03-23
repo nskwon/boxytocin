d148bb21d570a2f44fe4db4f88832394c58cdb3f
nskwon <nskwon@ucsd.edu>
2020-03-23 15:33:57 -0700

    Created Basic Enemy

---
+++ Boxytocin Project/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private BoxCollider2D box;
+    private bool cannotMove;
+    private Vector2 movement;
+    public int health = 50;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cannotMove = CheckCollisions(box, movement, 0.05f);
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (cannotMove)
+        {
+            movement.x = 0;
+            movement.y = 0;
+        }
+    }
+
+    private bool CheckCollisions(Collider2D moveCollider, Vector2 direction, float distance)
+    {
+        if (moveCollider != null)
+        {
+            RaycastHit2D[] hits = new RaycastHit2D[10];
+            ContactFilter2D filter = new ContactFilter2D() { };
+
+            int numHits = moveCollider.Cast(direction, filter, hits, distance);
+            for (int i = 0; i < numHits; i++)
+            {
+                if (!hits[i].collider.isTrigger)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public void takeDamage(int damage)
+    {
+        Debug.Log("Enemy health" + health);
+        health -= damage;
+    }
+}
