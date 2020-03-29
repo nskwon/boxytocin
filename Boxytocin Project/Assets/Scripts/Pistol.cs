af47e995ea953da7b412776657d60b3a94daf38c
nskwon <nskwon@ucsd.edu>
2020-03-29 15:46:53 -0700

    Created ammo counter
    Need to make a master weapons class to hold all instances of weapons

--- Boxytocin Project/Assets/Scripts/Pistol.cs
+++ Boxytocin Project/Assets/Scripts/Pistol.cs
@@ -11,16 +11,21 @@ public class Pistol : MonoBehaviour
     public float timeBetweenShots = 0.5f;
     private float timestamp;
     public int maxAmmo = 10;
-    public int currentAmmo = -1;
+    public static int currentAmmoPlayer1 = -1;
+    public static int currentAmmoPlayer2 = -1;
     public float timeBetweenReload = 1.0f;
     public float reloadTime = 3.0f;
     private bool isReloading = false;
 
     private void Start()
     {
-        if(currentAmmo == -1)
+        if(currentAmmoPlayer1 == -1 && transform.parent.tag == "Player1")
         {
-            currentAmmo = maxAmmo;
+            currentAmmoPlayer1 = maxAmmo;
+        }
+        if(currentAmmoPlayer2 == -1 && transform.parent.tag == "Player2")
+        {
+            currentAmmoPlayer2 = maxAmmo;
         }
 
     }
@@ -36,26 +41,26 @@ public class Pistol : MonoBehaviour
                     return;
                 }
 
-                if (currentAmmo <= 0)
+                if (currentAmmoPlayer1 <= 0)
                 {
-                    StartCoroutine(Reload());
+                    StartCoroutine(Reload1());
                     return;
                 }
                 if (Time.time >= timestamp && transform.parent.tag == "Player1" && Input.GetButton("Fire1"))
                 {
-                    StartCoroutine(Shoot());
+                    StartCoroutine(Shoot1());
                     timestamp = Time.time + timeBetweenShots;
-                    Debug.Log(currentAmmo);
+                    Debug.Log(currentAmmoPlayer1);
                 }
                 else if (Time.time >= timestamp && transform.parent.tag == "Player2" && Input.GetButton("Fire2"))
                 {
-                    StartCoroutine(Shoot());
+                    StartCoroutine(Shoot1());
                     timestamp = Time.time + timeBetweenShots;
                 }
             }
             else if(!Player1Script.alive && transform.parent.tag == "Player1")
             {
-                refillAmmo();
+                refillAmmo1();
                 return;
             }
 
@@ -67,36 +72,36 @@ public class Pistol : MonoBehaviour
                     return;
                 }
 
-                if (currentAmmo <= 0)
+                if (currentAmmoPlayer2 <= 0)
                 {
-                    StartCoroutine(Reload());
+                    StartCoroutine(Reload2());
                     return;
                 }
                 if (Time.time >= timestamp && transform.parent.tag == "Player1" && Input.GetButton("Fire1"))
                 {
-                    StartCoroutine(Shoot());
+                    StartCoroutine(Shoot2());
                     timestamp = Time.time + timeBetweenShots;
-                    Debug.Log(currentAmmo);
+                    Debug.Log(currentAmmoPlayer2);
                 }
                 else if (Time.time >= timestamp && transform.parent.tag == "Player2" && Input.GetButton("Fire2"))
                 {
-                    StartCoroutine(Shoot());
+                    StartCoroutine(Shoot2());
                     timestamp = Time.time + timeBetweenShots;
                 }
             }
             else if (!Player2Script.alive && transform.parent.tag == "Player2")
             {
-                refillAmmo();
+                refillAmmo2();
                 return;
             }
         }
     }
 
-    IEnumerator Shoot()
+    IEnumerator Shoot1()
     {
         //Shooting logic
         RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
-        currentAmmo--;
+        currentAmmoPlayer1--;
         if(hitInfo)
         {
 	    
@@ -130,19 +135,72 @@ public class Pistol : MonoBehaviour
 
     }
 
-    IEnumerator Reload()
+    IEnumerator Shoot2()
+    {
+        //Shooting logic
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+        currentAmmoPlayer2--;
+        if (hitInfo)
+        {
+
+            Debug.Log("Hit Object");
+            if (hitInfo.collider.tag == "Hittable" || hitInfo.collider.tag == "Player1" || hitInfo.collider.tag == "Player2")
+            {
+                Debug.Log("Take Damage");
+                hitInfo.transform.SendMessageUpwards("takeDamage", damage);
+            }
+            //Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+            lineRenderer.SetPosition(0, firePoint.position);
+            Debug.Log("Line sent out");
+            lineRenderer.SetPosition(1, hitInfo.point);
+            Debug.Log("Position set");
+            FindObjectOfType<AudioManager>().Play("PistolShoot");
+        }
+
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+            FindObjectOfType<AudioManager>().Play("PistolShoot");
+        }
+
+        lineRenderer.enabled = true;
+
+        //wait one frame
+        yield return new WaitForSeconds(0.02f);
+
+        lineRenderer.enabled = false;
+
+    }
+
+    IEnumerator Reload1()
     {
         FindObjectOfType<AudioManager>().Play("PistolReload");
         isReloading = true;
         Debug.Log("Reloading...");
         yield return new WaitForSeconds(reloadTime);
-        currentAmmo = maxAmmo;
+        currentAmmoPlayer1 = maxAmmo;
         isReloading = false;
     }
 
-    public void refillAmmo()
+    IEnumerator Reload2()
     {
-        currentAmmo = maxAmmo;
+        FindObjectOfType<AudioManager>().Play("PistolReload");
+        isReloading = true;
+        Debug.Log("Reloading...");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmoPlayer2 = maxAmmo;
+        isReloading = false;
+    }
+
+    public void refillAmmo1()
+    {
+        currentAmmoPlayer1 = maxAmmo;
+    }
+
+    public void refillAmmo2()
+    {
+        currentAmmoPlayer2 = maxAmmo;
     }
 
 }
